800876fb4707e3e55746f5e8c2268c4eb05cf305
saeyoon <ylyy44@naver.com>
2022-09-01 16:44:47 +0900

    유효성검사 스크립트작성

--- Web-study-11/src/main/java/com/saeyan/dao/BoardDAO.java
+++ Web-study-11/src/main/java/com/saeyan/dao/BoardDAO.java
@@ -56,30 +56,159 @@ public class BoardDAO {
 
 		return list;
 	}
-	
+
 	public void insertBoard(BoardVO bVo) {
-		String sql = "insert into bboard("
-				+ "num, name, email, pass, title, content) "
+		String sql = "insert into bboard(" + "num, name, email, pass, title, content) "
 				+ "values(board.seq.nextval, ?, ?, ?, ?, ?";
-		
+
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		
+
 		try {
 			conn = DBManager.getConnection();
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setString(1, bVo.getName());
 			pstmt.setString(2, bVo.getEmail());
 			pstmt.setString(3, bVo.getPass());
 			pstmt.setString(4, bVo.getTitle());
 			pstmt.setString(5, bVo.getContent());
-			
+
 			pstmt.executeUpdate();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			DBManager.close(conn, pstmt);
+		}
+	}
+
+	public void updateReadCount(String num) {
+		String sql = "update bboard set readcount=readcount+1 where num=?";
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, num);
+
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt);
+		}
+	}
+
+	public BoardVO selectOneBoardByNum(String num) {
+		String sql = "select * from bboard where num = ?";
+
+		BoardVO bVo = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, num);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				bVo = new BoardVO();
+				bVo.setNum(rs.getInt("num"));
+				bVo.setName(rs.getString("name"));
+				bVo.setPass(rs.getString("pass"));
+				bVo.setEmail(rs.getString("email"));
+				bVo.setTitle(rs.getString("title"));
+				bVo.setContent(rs.getString("content"));
+				bVo.setWritedate(rs.getTimestamp("writedate"));
+				bVo.setReadcount(rs.getInt("readcount"));
+			}
+		} catch (Exception e) {
+			DBManager.close(conn, pstmt, rs);
+		} finally {
+			DBManager.close(conn, pstmt, rs);
+		}
+
+		return bVo;
+	}
+
+	public void updateBoard(BoardVO bVo) {
+		String sql = "update board set name=?, email=?, pass=?, " + "title=?, content=?, where num=?";
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, bVo.getName());
+			pstmt.setString(2, bVo.getEmail());
+			pstmt.setString(3, bVo.getPass());
+			pstmt.setString(4, bVo.getTitle());
+			pstmt.setString(5, bVo.getContent());
+			pstmt.setInt(6, bVo.getNum());
+
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt);
+		}
+	}
+
+	public BoardVO checkPassWord(String pass, String num) {
+		String sql = "select * from bboard where pass=? and num=?";
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		BoardVO bVo = null;
+
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, pass);
+			pstmt.setString(2, num);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				bVo = new BoardVO();
+
+				bVo.setNum(rs.getInt("num"));
+				bVo.setName(rs.getString("name"));
+				bVo.setEmail(rs.getString("email"));
+				bVo.setPass(rs.getString("pass"));
+				bVo.setTitle(rs.getString("title"));
+				bVo.setContent(rs.getString("content"));
+				bVo.setReadcount(rs.getInt("readcount"));
+				bVo.setWritedate(rs.getTimestamp("writedate"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return bVo;
+	}
+
+	public void deleteBoard(String num) {
+		String sql = "delete bboard where num=?";
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, num);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
